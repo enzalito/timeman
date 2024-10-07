c0dddce1129b9608fb178eec5f0e93469a060ab3
Enzo Einhorn <enzo.einhorn@epitech.eu>
2024-10-07 14:59:34 +0200

    fix compose

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :timeman, Timeman.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "db",
+  hostname: "localhost",
   database: "timeman_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
