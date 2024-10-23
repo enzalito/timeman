de726925d57e13cc9af24d300489d719c64b2ce7
Cl <charles-louis.velieu@tutanota.com>
2024-10-23 12:39:21 +0200

    delete routes create and delete for working time

--- backend/lib/timeman_web/controllers/working_time_controller.ex
+++ backend/lib/timeman_web/controllers/working_time_controller.ex
@@ -7,26 +7,6 @@ defmodule TimemanWeb.WorkingTimeController do
 
   action_fallback TimemanWeb.FallbackController
 
-  # def create(conn, %{"userId" => userId, "working_time" => working_time_params}) do
-  #   with {:ok, %WorkingTime{} = working_time} <- Work.create_working_time(working_time_params) do
-  #     conn
-  #     |> put_status(:created)
-  #     |> put_resp_header("location", ~p"/api/workingtime/#{working_time}")
-  #     |> render(:show, working_time: working_time)
-  #   end
-  # end
-
-  def createWithUserRelation(conn, %{"user_id" => user_id, "working_time" => working_time}) do
-    working_time = Map.put(working_time, "user_id", user_id)
-    with {:ok, %WorkingTime{} = res} <-
-           Work.create_working_time(working_time) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/workingtime/#{res.user_id}")
-      |> render(:show, working_time: res)
-    end
-  end
-
   def getWorkingTime(conn, %{"user_id" => user_id, "id" => id}) do
     working_time_by_user = Work.get_working_time_by_Id_UserId!(user_id)
 
@@ -54,22 +34,16 @@ defmodule TimemanWeb.WorkingTimeController do
     render(conn, :show, working_time: working_time)
   end
 
-  def update(conn, %{"id" => id, "working_time" => working_time_params}) do
+  def update(conn, %{"id" => id, "working_time" => %{"description" => description}}) do
     working_time = Work.get_working_time!(id)
 
+    changeset = WorkingTime.changeset(working_time, %{"description" => description})
     with {:ok, %WorkingTime{} = working_time} <-
-           Work.update_working_time(working_time, working_time_params) do
+           Work.update_working_time(working_time, %{"description" => description}) do
       render(conn, :show, working_time: working_time)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    working_time = Work.get_working_time!(id)
-
-    with {:ok, %WorkingTime{}} <- Work.delete_working_time(working_time) do
-      send_resp(conn, :no_content, "")
-    end
-  end
 
   def swagger_definitions do
     %{
@@ -228,13 +202,4 @@ defmodule TimemanWeb.WorkingTimeController do
     )
   end
 
-  swagger_path :delete do
-    PhoenixSwagger.Path.delete("/api/workingtime/{id}")
-    summary("Delete working time")
-    produces("application/json")
-    deprecated(false)
-    parameter(:id, :path, :number, "working time id", required: true, example: 1)
-
-    response(204, "OK")
-  end
 end
