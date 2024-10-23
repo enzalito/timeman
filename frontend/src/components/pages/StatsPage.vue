b4ac7e4e79ec2d964ab1c8b92239043317eea2ff
Enzo Einhorn <enzo.einhorn@epitech.eu>
2024-10-23 14:57:42 +0200

    fix week summary components

--- frontend/src/components/pages/StatsPage.vue
+++ frontend/src/components/pages/StatsPage.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user';
-import WeekSummaryTeamView from '../charts/WeekSummaryTeamView.vue';
-import WeekSummary from '../charts/WeekSummary.vue';
-
+import { useUserStore } from "@/stores/user"
+import WeekSummaryTeamView from "@/components/charts/WeekSummaryTeam.vue"
+import WeekSummary from "@/components/charts/WeekSummary.vue"
 
 const userStore = useUserStore()
-
 </script>
 
 <template>
   <WeekSummary v-if="userStore?.user?.role === 'employee'" :userId="userStore?.user?.id" />
-  <WeekSummaryTeamView v-if="userStore?.user?.role === 'manager'" :team-id="1"/>
+  <!-- FIXME -->
+  <WeekSummaryTeamView v-if="userStore?.user?.role === 'manager'" :team-id="1" />
 </template>
