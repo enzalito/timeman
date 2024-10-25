d0261e2a97c73ed10d473154e04ffe2eb60ebf36
louis lasserre <louis.lasserre@mmibordeaux.com>
2024-10-25 16:59:02 +0200

    mobile side bar for teams

---
+++ frontend/src/components/SideDrawer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+
+
+
+
+  const {isOpen, classes} = defineProps<{isOpen: boolean, classes?: string}>()
+</script>
+
+
+<template>
+  <div :class="cn(`h-full w-[100vw] bg-white p-4 absolute right-0 top-0 bottom-0 transition-all translate-x-full ${classes} `, isOpen === true && 'translate-x-0')">
+    <slot/>
+  </div>
+</template>
