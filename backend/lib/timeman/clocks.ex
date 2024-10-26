fa72450c65e9d744d5c62f5c89be01e71b00bbe3
Cl <charles-louis.velieu@tutanota.com>
2024-10-26 15:58:26 +0200

    Fix bug split at 00:00 for wt

--- backend/lib/timeman/clocks.ex
+++ backend/lib/timeman/clocks.ex
@@ -101,6 +101,21 @@ defmodule Timeman.Clocks do
     wt3 = working_time
 
     cond do
+      start_time.day !== end_time.day ->
+        wt1 = %{working_time | end: %NaiveDateTime{start_time | hour: 23, minute: 59, second: 59}}
+
+        wt2 = %{
+          working_time
+          | start:
+              NaiveDateTime.add(start_time, 1 * 86_400)
+              |> Map.put(:hour, 0)
+              |> Map.put(:minute, 0)
+              |> Map.put(:second, 0)
+        }
+
+        add_working_time(wt1)
+        add_working_time(wt2)
+
       start_time.hour < 6 && end_time.hour >= 6 && start_time.day == end_time.day ->
         new_end_time = %NaiveDateTime{end_time | hour: 5, minute: 59}
         wt1 = %{working_time | end: new_end_time}
@@ -119,42 +134,6 @@ defmodule Timeman.Clocks do
 
         Work.create_working_time(wt1)
         add_working_time(wt2)
-
-      start_time.hour < 6 && start_time.day != end_time.day ->
-        new_end_time = %NaiveDateTime{end_time | hour: 5, minute: 59}
-        wt1 = %{working_time | end: new_end_time}
-
-        new_start_time = %NaiveDateTime{start_time | hour: 6, minute: 00}
-        wt2_end_time = %NaiveDateTime{start_time | hour: 23, minute: 59}
-        wt2 = %{working_time | start: new_start_time, end: wt2_end_time}
-
-        wt3_start_time = %NaiveDateTime{start_time | hour: 0, minute: 0}
-
-        wt3_start_time =
-          NaiveDateTime.add(start_time, 1 * 86_400) |> Map.put(:hour, 0) |> Map.put(:minute, 0)
-
-        wt3 = %{working_time | start: wt3_start_time}
-
-        Work.create_working_time(wt1)
-        add_working_time(wt2)
-        add_working_time(wt3)
-
-      start_time.hour < 22 && start_time.day != end_time.day ->
-        new_end_time = %NaiveDateTime{end_time | hour: 21, minute: 59}
-        wt1 = %{working_time | end: new_end_time}
-
-        new_start_time = %NaiveDateTime{start_time | hour: 22, minute: 00}
-        wt2_end_time = %NaiveDateTime{start_time | hour: 23, minute: 59}
-        wt2 = %{working_time | start: new_start_time, end: wt2_end_time}
-
-        wt3_start_time =
-          NaiveDateTime.add(start_time, 1 * 86_400) |> Map.put(:hour, 0) |> Map.put(:minute, 0)
-
-        wt3 = %{working_time | start: wt3_start_time}
-
-        Work.create_working_time(wt1)
-        Work.create_working_time(wt2)
-        add_working_time(wt3)
 
       true ->
         Work.create_working_time(working_time)
